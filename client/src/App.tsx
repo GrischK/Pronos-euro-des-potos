1b23bcc52dce2d078fef76ad22f1118164051845
GrischK <grischka.gorski@laposte.net>
2024-04-01 13:34:04 +0200

    ✨ Log out

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,14 +3,23 @@ import {Route, Routes} from "react-router-dom";
 import HomePage from "./screens/HomePage/HomePage";
 import SignUp from "./screens/SignUp/SignUp";
 import Login from "./screens/Login/Login";
+import {useGetProfileQuery} from "./gql/generated/schema";
+import Home from "./screens/Home/Home";
 
 function App() {
+    const {data: current, refetch} = useGetProfileQuery({errorPolicy: "ignore",});
+    const userIsLogged = current?.profile?.id
 
     return (
         <Routes>
             <Route path={'/'} element={<HomePage/>}/>
             <Route path={'/sign-up'} element={<SignUp/>}/>
             <Route path={'/login'} element={<Login/>}/>
+            {
+                userIsLogged && (
+                    <Route path={'/home'} element={<Home/>}/>
+                )
+            }
         </Routes>
     );
 }
