60c829dffa107fdc8f04415ed705d5d8706a11b3
GrischK <grischka.gorski@laposte.net>
2024-04-29 01:13:39 +0200

    💄 Add border

--- client/src/components/ui/3d-card-component.tsx
+++ client/src/components/ui/3d-card-component.tsx
@@ -4,7 +4,7 @@ import { CardBody, CardContainer, CardItem } from "./3d-card";
 export default function ThreeDCardDemo({ points }: any) {
   return (
     <CardContainer className="inter-var cursor-pointer">
-      <CardBody className="flex flex-col justify-center items-center relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] sm:w-[15vh] sm:h-[15vh] rounded-full p-6 border bg-[#0f172a]">
+      <CardBody className="flex flex-col justify-center items-center relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] sm:w-[15vh] sm:h-[15vh] rounded-full p-6 border bg-[#0f172a] border-solid border-white border-opacity-20">
         <CardItem
           translateZ="100"
           className="w-full text-center flex flex-col justify-center items-center"
