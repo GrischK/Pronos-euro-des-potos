490c4cd592a9d70322a62270ec241455f4b2d1fc
GrischK <grischka.gorski@laposte.net>
2024-05-02 10:36:50 +0200

    ♻️ Refacto

--- client/src/components/ui/Button-hover-gradient.tsx
+++ client/src/components/ui/Button-hover-gradient.tsx
@@ -1,37 +1,38 @@
 import * as React from "react";
-import {MouseEventHandler, ReactNode} from "react";
-import {cn} from "../../utils/cn";
+import { MouseEventHandler, ReactNode } from "react";
+import { cn } from "../../utils/cn";
 
 interface ButtonHoverGradientProps {
-    children: ReactNode;
-    onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
-    className?: string,
+  children: ReactNode;
+  onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
+  className?: string;
 }
 
 export default function ButtonHoverGradient({
-                                                children,
-                                                onClick,
-                                                className
-                                            }: ButtonHoverGradientProps) {
-    const BottomGradient = () => {
-        return (
-            <>
-                <span
-                    className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent transform -translate-x-1/2 left-1/2 w-4/5"/>
-                <span
-                    className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent"/>
-            </>
-        );
-    };
-
+  children,
+  onClick,
+  className,
+}: ButtonHoverGradientProps) {
+  const BottomGradient = () => {
     return (
-        <button
-            className={cn("bg-slate-900/[0.8] relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 dark:bg-zinc-800 w-full text-white rounded-full h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset] p-4 flex items-center justify-center gap-4", className)}
-            type="submit"
-            onClick={onClick}
-        >
-            {children}
-            <BottomGradient/>
-        </button>
+      <>
+        <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent transform -translate-x-1/2 left-1/2 w-4/5" />
+        <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+      </>
     );
+  };
+
+  return (
+    <button
+      className={cn(
+        "bg-slate-900/[0.8] relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 dark:bg-zinc-800 w-full text-white rounded-full h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset] p-4 flex items-center justify-center gap-4",
+        className,
+      )}
+      type="submit"
+      onClick={onClick}
+    >
+      {children}
+      <BottomGradient />
+    </button>
+  );
 }
