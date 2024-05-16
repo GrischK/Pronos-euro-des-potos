bb9221c39c57741491f86c2418d997b812765ac6
GrischK <grischka.gorski@laposte.net>
2024-05-16 16:16:31 +0200

    💄Change home buttons

---
+++ client/src/components/ui/Shimmer-button/Shimmer-button.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./Shimmer-button.module.css";
+
+interface ShimmerButtonProps {
+  children: string;
+}
+
+export function ShimmerButton({ children }: ShimmerButtonProps) {
+  return (
+    <button
+      className={`relative text-xl inline-flex h-12 items-center justify-center rounded-full border m-1 border-slate-800 bg-[linear-gradient(110deg,#0f172a,45%,#1e2631,55%,#0f172a)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-red-300 ${styles.shimmer_button}`}
+    >
+      {children}
+    </button>
+  );
+}
