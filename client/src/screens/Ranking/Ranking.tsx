3c2e089a79c7fb8801293edc3f5d3f964fce0805
GrischK <grischka.gorski@laposte.net>
2024-06-18 00:09:22 +0200

    💄 Style changes
      Profile button position
      First players on ranking

--- client/src/screens/Ranking/Ranking.tsx
+++ client/src/screens/Ranking/Ranking.tsx
@@ -11,6 +11,8 @@ import {
 import { fetchUserImages, points } from "../../utils/functions";
 import { StickyScrollRevealDemo } from "../../components/ui/Sticky-scroll-reveal-component";
 import RefreshRoundedIcon from "@mui/icons-material/RefreshRounded";
+import { AnimatedTooltipPreview } from "../../components/ui/Animated-tooltip-preview";
+import Cup from "./cup.png";
 
 export default function Ranking() {
   const { data: allPredictions, refetch: refetchAllPredictions } =
@@ -62,10 +64,31 @@ export default function Ranking() {
     .sort((a, b) => (a.points || 0) - (b.points || 0))
     .reverse();
 
-  console.log(refresh);
+  const rankings: { [points: number]: number } = {};
+
+  // On détermine la position dans le classement en fonction des points
+  // Comme ça si les potos ont le même nombre de points, ils sont ex aequo dans le classment
+  sortedUsers.forEach((contentInfo: any, index: number) => {
+    const points = contentInfo.points;
+    if (rankings[points] === undefined) {
+      rankings[points] = index + 1;
+    }
+  });
+
+  // Trouve les points correspondant au premier rang
+  const topRankPoints = Object.keys(rankings).find(
+    (key) => rankings[Number(key)] === 1,
+  );
+
+  // Filtre les joueurs ayant les points correspondant au premier rang
+  const topRankUsers = sortedUsers.filter(
+    (user) => user.points && user.points === Number(topRankPoints),
+  );
+
+  console.log(topRankUsers);
   return (
     <div className={styles.ranking_container}>
-      <div className="h-[60vh] md:h-[40rem] w-full rounded-md flex md:items-center md:justify-center antialiased relative overflow-hidden">
+      <div className="h-[60vh] md:h-[40rem] w-full rounded-md flex md:items-center md:justify-center antialiased relative overflow-hidden flex-col">
         <Spotlight
           className="-top-40 left-0 md:left-60 md:-top-20"
           fill="white"
@@ -76,6 +99,13 @@ export default function Ranking() {
             <h1 className={styles.title_slim}>&nbsp;des potos</h1>
           </div>
         </div>
+        <img
+          style={{ height: "150px", marginTop: "5vh" }}
+          src={Cup}
+          alt={"cup"}
+        />
+
+        <AnimatedTooltipPreview champions={topRankUsers} />
       </div>
       {/*<div className={styles.usersList_container}>*/}
       {/*  {sortedUsers.map((user) => (*/}
