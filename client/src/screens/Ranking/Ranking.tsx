19771ffe0b9fb0bfbe9d5e08b3963793b0e691da
GrischK <grischka.gorski@laposte.net>
2024-06-18 13:45:58 +0200

    💄 Amend home page
      Amend lamp position
      Add crown on ranking page

--- client/src/screens/Ranking/Ranking.tsx
+++ client/src/screens/Ranking/Ranking.tsx
@@ -12,7 +12,6 @@ import { fetchUserImages, points } from "../../utils/functions";
 import { StickyScrollRevealDemo } from "../../components/ui/Sticky-scroll-reveal-component";
 import RefreshRoundedIcon from "@mui/icons-material/RefreshRounded";
 import { AnimatedTooltipPreview } from "../../components/ui/Animated-tooltip-preview";
-import Cup from "./cup.png";
 
 export default function Ranking() {
   const { data: allPredictions, refetch: refetchAllPredictions } =
@@ -85,7 +84,6 @@ export default function Ranking() {
     (user) => user.points && user.points === Number(topRankPoints),
   );
 
-  console.log(topRankUsers);
   return (
     <div className={styles.ranking_container}>
       <div className="h-[60vh] md:h-[40rem] w-full rounded-md flex md:items-center md:justify-center antialiased relative overflow-hidden flex-col">
@@ -93,49 +91,18 @@ export default function Ranking() {
           className="-top-40 left-0 md:left-60 md:-top-20"
           fill="white"
         />
-        <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-[10rem] md:pt-0">
+        <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-[6rem] md:pt-0">
           <div className={styles.title_container}>
             <h1 className={styles.title}>Classement</h1>
             <h1 className={styles.title_slim}>&nbsp;des potos</h1>
           </div>
         </div>
-        <img
-          style={{ height: "150px", marginTop: "5vh" }}
-          src={Cup}
-          alt={"cup"}
-        />
+        <div className={styles.champion_container}>
+          <span>👑</span>
+        </div>
 
         <AnimatedTooltipPreview champions={topRankUsers} />
       </div>
-      {/*<div className={styles.usersList_container}>*/}
-      {/*  {sortedUsers.map((user) => (*/}
-      {/*    <div key={user.id} className={styles.userDetails_container}>*/}
-      {/*      <span className={styles.userName}>{user.name}</span>*/}
-      {/*      <div>*/}
-      {/*        <CountUp*/}
-      {/*          className={"text-6xl text-white"}*/}
-      {/*          start={0}*/}
-      {/*          end={user.points || 0}*/}
-      {/*          duration={2.5}*/}
-      {/*          delay={1}*/}
-      {/*        />*/}
-      {/*        <span className={"text-sm text-white"}>points</span>*/}
-      {/*      </div>*/}
-      {/*      {user.picture ? (*/}
-      {/*        <img*/}
-      {/*          className={styles.my_avatar}*/}
-      {/*          src={user.picture}*/}
-      {/*          alt={user.name}*/}
-      {/*        />*/}
-      {/*      ) : (*/}
-      {/*        <div className={styles.generic_avatar}>*/}
-      {/*          <SoccerPlayer />*/}
-      {/*        </div>*/}
-      {/*      )}*/}
-      {/*    </div>*/}
-      {/*  ))}*/}
-      {/*</div>*/}
-      {/*{sortedUsers.length && <TabsDemo tabsContent={sortedUsers} />}*/}
       {sortedUsers.length && (
         <StickyScrollRevealDemo contentData={sortedUsers} />
       )}
