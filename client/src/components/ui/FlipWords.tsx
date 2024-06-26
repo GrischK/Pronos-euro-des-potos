49c48bf8292cbc9155ae43e574321b8e2fcbd235
GrischK <grischka.gorski@laposte.net>
2024-06-26 14:12:25 +0200

    💬 Change text for ranking button

--- client/src/components/ui/FlipWords.tsx
+++ client/src/components/ui/FlipWords.tsx
@@ -64,20 +64,34 @@ export const FlipWords = ({
         )}
         key={currentWord}
       >
-        {currentWord.split("").map((letter, index) => (
+        {currentWord !== "emoji" ? (
+          currentWord.split("").map((letter, index) => (
+            <motion.span
+              key={currentWord + index}
+              initial={{ opacity: 0, y: 10, filter: "blur(8px)" }}
+              animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
+              transition={{
+                delay: index * 0.08,
+                duration: 0.4,
+              }}
+              className="inline-block"
+            >
+              {letter}
+            </motion.span>
+          ))
+        ) : (
           <motion.span
-            key={currentWord + index}
             initial={{ opacity: 0, y: 10, filter: "blur(8px)" }}
             animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
             transition={{
-              delay: index * 0.08,
+              delay: 1 * 0.08,
               duration: 0.4,
             }}
             className="inline-block"
           >
-            {letter}
+            🥳🤩
           </motion.span>
-        ))}
+        )}
       </motion.div>
     </AnimatePresence>
   );
